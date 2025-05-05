0174caecf6560da5f85c9e60ca0c2cc1bde8f2ef
NormalFall <93881091+NormalFall@users.noreply.github.com>
2025-05-05 11:24:15 -0400

    Add basic docs for the game logic + sort function inside TetrisGame

--- src/main.rs
+++ src/main.rs
@@ -135,26 +135,42 @@ fn main() -> Result<(), String> {
     Ok(())
 }
 
+
+/// Basic position struct for position handeling
 #[derive(Clone, Copy)]
 struct Position {
     x: i32,
     y: i32
 }
 
+/// This struct contains all game logic and game states
 struct TetrisGame<'a> {
+    /// surface to draw on
     canvas: &'a mut Canvas<Window>,
+    /// number of lines cleared
     score: usize,
-    board: [[Option<Color>; GAME_WIDTH]; GAME_HEIGHT], // Official tetris game size
+    /// game grid. Uses GAME_WIDTH and GAME_HEIGHT for size
+    /// 
+    /// Only contains the placed squares with thier colors. Not the tetris themselves
+    board: [[Option<Color>; GAME_WIDTH]; GAME_HEIGHT],
+    /// Number of pixel each board squares uses
     size_ratio: u32, // Size in pixel of each square
+    /// rng generator
     rng: SmallRng,
+    /// Acelerates when player is pressing down
     fast_falling: bool,
+    /// Number of times counter needs to increment so that a game update happens
     refresh_speed: usize,
+    /// Counter for time passing
     refresh_counter: usize,
+    /// Contains the struct of the current falling tetris
     current_tetris: Tetris,
+    /// When true main loop will exit game
     gameover: bool
 }
 
 impl<'a> TetrisGame<'a> {
+    /// Creates a new TetrisGame object
     fn new(canvas: &'a mut Canvas<Window>, seed: u64) -> Self {
         Self {
             canvas,
@@ -170,6 +186,7 @@ impl<'a> TetrisGame<'a> {
         }
     }
 
+    /// Creates a new current tetris with rng
     fn make_new_random_tetris(&mut self) {
         let colors = self.rng.next_u32().to_be_bytes();
         let map: TetrisMap = match self.rng.next_u32() % 8 {
@@ -186,6 +203,9 @@ impl<'a> TetrisGame<'a> {
         self.current_tetris = Tetris::new(map, Color::RGB(colors[0], colors[1], colors[3]));
     }
 
+    /// Function managing game speed and fastfall
+    /// 
+    /// Current behavior is that the games speeds up at each 2 lines clears
     fn update_timer(&mut self) -> Result<(), String> {
         let refresh_speed = {
             if self.fast_falling {
@@ -201,104 +221,6 @@ impl<'a> TetrisGame<'a> {
             self.refresh_counter = 0;
         }
         Ok(())
-    }
-
-    fn draw_refresh_all(&mut self) -> Result<(), String> {
-        self.canvas.clear(); // Erase everything
-
-        // Redraw board
-        for (yindex, y) in self.board.into_iter().enumerate() {
-            for (xindex, x) in y.into_iter().enumerate() {
-                if let Some(c) = x {
-                    self.canvas.set_draw_color(c);
-                    self.draw_in_grid(Position {x: xindex as i32, y: yindex as i32})?;
-                } else {
-                    self.canvas.set_draw_color(Color::RGB(0, 0, 0));
-                    self.draw_in_grid(Position {x: xindex as i32, y: yindex as i32})?; 
-                }
-            }
-        }
-
-        self.draw_tetris()?;
-
-        Ok(())
-    }
-
-    fn erase_tetris(&mut self) -> Result<(), String> {
-        self.draw_tetris_pick_color(Color::RGB(0, 0, 0))
-    }
-
-    fn draw_tetris_pick_color(&mut self, color: Color) -> Result<(), String> {
-        self.canvas.set_draw_color(color);
-        for (yindex, y) in self.current_tetris.map.into_iter().enumerate() {
-            for (xindex, x) in y.into_iter().enumerate() {
-                if x {
-                    self.draw_in_grid(Position {x :(xindex as i32 + self.current_tetris.position.x), y: (yindex as i32 + self.current_tetris.position.y)})?;
-                }
-                
-            }
-        }
-        Ok(())
-    }
-
-    fn draw_tetris(&mut self) -> Result<(), String> {
-        self.draw_tetris_pick_color(self.current_tetris.color)
-    }
-
-    fn check_tetris_hit_board(&self, tetris: &Tetris, shift: Position) -> bool {
-        let mut colided = false;
-        for (yindex, y) in tetris.map.into_iter().enumerate() {
-            for (xindex, x) in y.into_iter().enumerate() {
-                if x &&
-                    (yindex as i32 + shift.y + tetris.position.y) >= 0 &&
-                    (yindex as i32 + shift.y + tetris.position.y) < GAME_HEIGHT as i32 &&
-                    (xindex as i32 + shift.x + tetris.position.x) >= 0 &&
-                    (xindex as i32 + shift.x + tetris.position.x) < GAME_WIDTH as i32 {
-                    if self.board[(yindex as i32 + shift.y + tetris.position.y) as usize][(xindex as i32 + shift.x + tetris.position.x) as usize] != None {
-                        colided = true;
-                    }
-                }
-            }
-        }
-
-        colided
-    }
-
-    fn check_tetris_hit_wall(&self, tetris: &Tetris, shift: i32) -> bool {
-        tetris.position.x + tetris.most_left().unwrap_or_default() as i32 + shift < 0 ||
-        tetris.position.x + tetris.most_right().unwrap_or_default() as i32 + shift >= GAME_WIDTH as i32
-    }
-
-    fn check_tetris_hit_bottom(&self, tetris: &Tetris, shift: i32) -> bool {
-        tetris.position.y + tetris.most_bottom().unwrap() as i32 + shift >= GAME_HEIGHT as i32
-    }
-
-    fn insert_tetris_in_map(&mut self) {
-        for (yindex, y) in self.current_tetris.map.into_iter().enumerate() {
-            for (xindex, x) in y.into_iter().enumerate() {
-                if x &&
-                    yindex as i32 + self.current_tetris.position.y >= 0 &&
-                    yindex as i32 + self.current_tetris.position.y < GAME_HEIGHT as i32 &&
-                    xindex as i32 + self.current_tetris.position.x >= 0 &&
-                    xindex as i32 + self.current_tetris.position.x < GAME_WIDTH as i32 {
-                    self.board[(yindex as i32 + self.current_tetris.position.y) as usize][(xindex as i32 + self.current_tetris.position.x) as usize] = Some(self.current_tetris.color);
-                }
-
-                if x && yindex as i32 + self.current_tetris.position.y < 0 {
-                    self.gameover = true;
-                }
-            }
-        } 
-    }
-
-    fn draw_in_grid(&mut self, postion: Position) -> Result<(), String> {
-        let rectangle = Rect::new(
-            postion.x * self.size_ratio as i32,
-            postion.y * self.size_ratio as i32 ,
-            self.size_ratio,
-            self.size_ratio
-        );
-        return self.canvas.fill_rect(rectangle);
     }
 
     fn update(&mut self) -> Result<(), String> {
@@ -328,6 +250,100 @@ impl<'a> TetrisGame<'a> {
         Ok(())
     }
 
+    /// Main draw function for the game grid
+    /// 
+    /// Draws a single square based on the position and uses ratio for each square size
+    fn draw_in_grid(&mut self, postion: Position) -> Result<(), String> {
+        let rectangle = Rect::new(
+            postion.x * self.size_ratio as i32,
+            postion.y * self.size_ratio as i32 ,
+            self.size_ratio,
+            self.size_ratio
+        );
+        return self.canvas.fill_rect(rectangle);
+    }
+
+
+    /// Redraws the whole game from scratch.
+    /// 
+    /// Avoid using this often
+    fn draw_refresh_all(&mut self) -> Result<(), String> {
+        self.canvas.clear(); // Erase everything
+
+        // Redraw board
+        for (yindex, y) in self.board.into_iter().enumerate() {
+            for (xindex, x) in y.into_iter().enumerate() {
+                if let Some(c) = x {
+                    self.canvas.set_draw_color(c);
+                    self.draw_in_grid(Position {x: xindex as i32, y: yindex as i32})?;
+                } else {
+                    self.canvas.set_draw_color(Color::RGB(0, 0, 0));
+                    self.draw_in_grid(Position {x: xindex as i32, y: yindex as i32})?; 
+                }
+            }
+        }
+
+        self.draw_tetris()?;
+
+        Ok(())
+    }
+
+    /// Draws current_tetris at its current position with the color of your choice
+    fn draw_tetris_pick_color(&mut self, color: Color) -> Result<(), String> {
+        self.canvas.set_draw_color(color);
+        for (yindex, y) in self.current_tetris.map.into_iter().enumerate() {
+            for (xindex, x) in y.into_iter().enumerate() {
+                if x {
+                    self.draw_in_grid(Position {x :(xindex as i32 + self.current_tetris.position.x), y: (yindex as i32 + self.current_tetris.position.y)})?;
+                }
+                
+            }
+        }
+        Ok(())
+    }
+
+    /// Draws black current_tetris at its current position
+    fn erase_tetris(&mut self) -> Result<(), String> {
+        self.draw_tetris_pick_color(Color::RGB(0, 0, 0))
+    }
+
+    /// Draws current_tetris at its current position with its intended color
+    fn draw_tetris(&mut self) -> Result<(), String> {
+        self.draw_tetris_pick_color(self.current_tetris.color)
+    }
+
+    /// Check if tetris will hit board when shifted
+    fn check_tetris_hit_board(&self, tetris: &Tetris, shift: Position) -> bool {
+        let mut colided = false;
+        for (yindex, y) in tetris.map.into_iter().enumerate() {
+            for (xindex, x) in y.into_iter().enumerate() {
+                if x &&
+                    (yindex as i32 + shift.y + tetris.position.y) >= 0 &&
+                    (yindex as i32 + shift.y + tetris.position.y) < GAME_HEIGHT as i32 &&
+                    (xindex as i32 + shift.x + tetris.position.x) >= 0 &&
+                    (xindex as i32 + shift.x + tetris.position.x) < GAME_WIDTH as i32 {
+                    if self.board[(yindex as i32 + shift.y + tetris.position.y) as usize][(xindex as i32 + shift.x + tetris.position.x) as usize] != None {
+                        colided = true;
+                    }
+                }
+            }
+        }
+
+        colided
+    }
+
+    /// Check if tetris is hitting the board's walls
+    fn check_tetris_hit_wall(&self, tetris: &Tetris, shift: i32) -> bool {
+        tetris.position.x + tetris.most_left().unwrap_or_default() as i32 + shift < 0 ||
+        tetris.position.x + tetris.most_right().unwrap_or_default() as i32 + shift >= GAME_WIDTH as i32
+    }
+
+    /// Check if tetris reached the bottom of the board
+    fn check_tetris_hit_bottom(&self, tetris: &Tetris, shift: i32) -> bool {
+        tetris.position.y + tetris.most_bottom().unwrap() as i32 + shift >= GAME_HEIGHT as i32
+    }
+
+    /// Move the tetris while redrawing it
     fn move_tetris(&mut self, shift: Position) -> Result<(), String> {
         self.erase_tetris()?;
         self.current_tetris.position.x += shift.x;
@@ -336,6 +352,9 @@ impl<'a> TetrisGame<'a> {
         Ok(())
     }
 
+    /// Move the tetris while redrawing it
+    /// 
+    /// Also check if the movement will hit something and cancels if it will something
     fn move_tetris_with_check(&mut self, shift: Position) -> Result<(), String> {
         if !self.check_tetris_hit_board(&self.current_tetris, shift) && !self.check_tetris_hit_wall(&self.current_tetris, shift.x) {
             self.move_tetris(shift)?;
@@ -344,6 +363,11 @@ impl<'a> TetrisGame<'a> {
         Ok(())
     }
 
+    /// Rotates the tetris left
+    /// 
+    /// Cancels the action if it will hit something if the rotation is done
+    /// 
+    /// Note: If you need to rotate without checks use the tetris struct directly
     fn rotate_tetris_left(&mut self) -> Result<(), String> {
         let rotated_tetris = Tetris {
             position: self.current_tetris.position,
@@ -360,6 +384,12 @@ impl<'a> TetrisGame<'a> {
         Ok(())
     }
 
+
+    /// Rotates the tetris left
+    /// 
+    /// Cancels the action if it will hit something if the rotation is done
+    /// 
+    /// Note: If you need to rotate without checks use the tetris struct directly
     fn rotate_tetris_right(&mut self) -> Result<(), String> {
         let rotated_tetris = Tetris {
             position: self.current_tetris.position,
@@ -376,6 +406,32 @@ impl<'a> TetrisGame<'a> {
         Ok(())
     }
 
+    /// Insert current_tetris inside board
+    fn insert_tetris_in_map(&mut self) {
+        for (yindex, y) in self.current_tetris.map.into_iter().enumerate() {
+            for (xindex, x) in y.into_iter().enumerate() {
+                if x &&
+                    yindex as i32 + self.current_tetris.position.y >= 0 &&
+                    yindex as i32 + self.current_tetris.position.y < GAME_HEIGHT as i32 &&
+                    xindex as i32 + self.current_tetris.position.x >= 0 &&
+                    xindex as i32 + self.current_tetris.position.x < GAME_WIDTH as i32 {
+                    self.board[(yindex as i32 + self.current_tetris.position.y) as usize][(xindex as i32 + self.current_tetris.position.x) as usize] = Some(self.current_tetris.color);
+                }
+
+                if x && yindex as i32 + self.current_tetris.position.y < 0 {
+                    self.gameover = true;
+                }
+            }
+        } 
+    }
+
+
+    /// Finds all of the full lines in boards.
+    /// 
+    /// Return an array with the false or true for each line.
+    /// True if the line is full
+    /// 
+    /// Important to have it like this because only give the full line indexes would use HEAP
     fn full_lines(&self) -> [bool; GAME_HEIGHT] {
         let mut full_lines = [false; GAME_HEIGHT];
         for (index, line) in self.board.into_iter().enumerate() {
@@ -387,6 +443,7 @@ impl<'a> TetrisGame<'a> {
         full_lines
     }
 
+    /// Deletes the specified line and shift all lines above it
     fn clear_line(&mut self, line_index: usize) {
         // Shift lines todo: dont use clone here
         for (index, line) in self.board.clone()[0..line_index].into_iter().enumerate() {
@@ -396,6 +453,7 @@ impl<'a> TetrisGame<'a> {
     }
 }
 
+/// Map type to make sure tetris management stays consistent
 type TetrisMap = [[bool; 5]; 5];
 
 const LINE: TetrisMap = [[false, false, false, false, false],
@@ -437,13 +495,18 @@ const SQUARE: TetrisMap = [[false, false, false, false, false],
                                 [false, false, false, false, false],
                                 [false, false, false, false, false]];
 
+/// Contains the tetris props and transform logics
 struct Tetris {
+    /// Current tetris position
     position: Position,
+    /// Contains the tetris shape
     map: TetrisMap,
+    /// Tetris color
     color: Color
 }
 
 impl Tetris {
+    /// Makes a new tetris object
     fn new(map: TetrisMap, color: Color) -> Self {
         Self {
             position: Position {x: (GAME_WIDTH as i32 / 2) - 2, y: -5},
@@ -452,6 +515,7 @@ impl Tetris {
         }
     }
 
+    /// Return the tetris map if it was rotated right
     fn rotate_right_result(&self) -> TetrisMap {
         let mut new_tetris: TetrisMap = [[false; 5]; 5];
         
@@ -464,6 +528,7 @@ impl Tetris {
         return new_tetris;
     }
 
+    /// Return the tetris map if it was rotated left
     fn rotate_left_result(&self) -> TetrisMap {
         let mut new_tetris: TetrisMap = [[false; 5]; 5];
         
@@ -476,14 +541,19 @@ impl Tetris {
         return new_tetris;
     }
 
+    /// Rotates the tetris map right directly
     fn rotate_right(&mut self) {
         self.map = self.rotate_right_result();
     }
 
+    /// Rotates the tetris map left directly
     fn rotate_left(&mut self) {
         self.map = self.rotate_left_result();
     }
 
+    /// Finds the most left square in map
+    /// 
+    /// returns the x index of it
     fn most_left(&self) -> Option<usize> {
         let mut pos: Option<usize> = None;
         for y in self.map.iter() {
@@ -497,6 +567,9 @@ impl Tetris {
         pos
     }
 
+    /// Finds the most right square in map
+    /// 
+    /// returns the x index of it
     fn most_right(&self) -> Option<usize> {
         let mut pos: Option<usize> = None;
         for y in self.map.iter() {
@@ -510,6 +583,9 @@ impl Tetris {
         pos
     }
 
+    /// Finds the most bottom square in map
+    /// 
+    /// returns the y index of it
     fn most_bottom(&self) -> Option<usize> {
         let mut pos: Option<usize> = None;
         for (yindex, y) in self.map.iter().enumerate() {
